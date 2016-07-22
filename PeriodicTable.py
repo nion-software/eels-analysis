6535108415da930fc373e906157fa9fe3bc907fa
Chris Meyer <cmeyer1969@gmail.com>
2016-07-22 12:55:18 -0700

    Add method to find edges within interval, sorted by distance from center.

--- PeriodicTable.py
+++ PeriodicTable.py
@@ -1,6 +1,7 @@
 # standard libraries
 import fractions
 import json
+import operator
 import os
 import typing
 
@@ -92,7 +93,7 @@ class PeriodicTable(metaclass=Singleton):
         """Return a list of tuples: atomic number, atomic symbol."""
         return ((edge_data_item.get("z"), edge_data_item.get("symbol")) for edge_data_item in self.__edge_data)
 
-    def get_edges_list(self, atomic_number: int) -> typing.Tuple[int, str]:
+    def get_edges_list(self, atomic_number: int) -> typing.Tuple[ElectronShell, str]:
         """Return a list of tuples: electron shell (lowest energy within shell number), edge name (without subshell)."""
         for edge_data_item in self.__edge_data:
             if edge_data_item.get("z", 0) == atomic_number:
@@ -106,7 +107,29 @@ class PeriodicTable(metaclass=Singleton):
                 return list((edge_map[key][0], edge_map[key][0].to_long_str()) for key in sorted(edge_map.keys()))
         return None
 
+    def find_edges_in_energy_interval(self, energy_interval_ev: typing.Tuple[float, float]) -> typing.List[ElectronShell]:
+        """Return list of electron shells found within energy interval, sorted by distance from center."""
+        edges = list()  # typing.List[typing.Tuple[float, ElectronShell]]
+        energy_interval_center_ev = (energy_interval_ev[0] + energy_interval_ev[1]) * 0.5
+        for edge_data_item in self.__edge_data:
+            atomic_number = edge_data_item.get("z", 0)
+            edge_dict = edge_data_item.get("edges", dict())
+            # find lowest energy edge within each shell
+            edge_map = dict()
+            for eels_shell, energy in edge_dict.items():
+                electron_shell = ElectronShell.from_eels_notation(atomic_number, eels_shell)
+                base_electron_shell = edge_map.setdefault(electron_shell.shell_number, (None, 1E9))
+                if energy < base_electron_shell[1]:
+                    edge_map[electron_shell.shell_number] = (electron_shell, energy)
+            for electron_shell, energy in edge_map.values():
+                if energy_interval_ev[0] <= energy <= energy_interval_ev[1]:
+                    edges.append((abs(energy_interval_center_ev - energy), electron_shell))
+        edges.sort(key=operator.itemgetter(0))
+        return [edge[1] for edge in edges]
+
+
 
 # print(ElectronShell.from_eels_notation(6, "M4"))
 # print(ElectronShell.from_eels_notation(6, "M4").get_shell_str_in_eels_notation(True))
 # print(PeriodicTable().nominal_binding_energy_ev(ElectronShell.from_eels_notation(35, "M4")))
+# print([es.to_long_str() for es in PeriodicTable().find_edges_in_energy_interval((1833, 1933))])
