1e3ff446575e774288133047d86531465e32970a
Chris Meyer <cmeyer1969@gmail.com>
2018-04-21 12:35:00 -0700

    Clean up setup.py files in preparation for PyPI.

--- setup.py
+++ setup.py
@@ -10,12 +10,13 @@ import os
 
 setuptools.setup(
     name="nionswift-eels-analysis",
-    version="0.0.1",
+    version="0.1.0",
     author="Nion Software",
     author_email="swift@nion.com",
     description="Library and UI for doing EELS analysis with Nion Swift.",
     url="https://github.com/nion-software/eels-analysis",
-    packages=["nion.eels_analysis", "nionswift_plugin.nion_eels_analysis"],
+    packages=["nion.eels_analysis", "nion.eels_analysis.test", "nionswift_plugin.nion_eels_analysis"],
+    package_data={"nion.eels_analysis": ["resources/*"]},
     install_requires=[],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
