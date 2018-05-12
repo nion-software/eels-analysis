3236bf499c21876c2c8b5bc90e47bffdcffdcec8
Chris Meyer <cmeyer1969@gmail.com>
2018-05-12 15:36:15 -0700

    Fix setup to include plugin test directory.

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ import os
 
 setuptools.setup(
     name="nionswift-eels-analysis",
-    version="0.1.0",
+    version="0.1.1",
     author="Nion Software",
     author_email="swift@nion.com",
     description="Library and UI for doing EELS analysis with Nion Swift.",
     url="https://github.com/nion-software/eels-analysis",
-    packages=["nion.eels_analysis", "nion.eels_analysis.test", "nionswift_plugin.nion_eels_analysis"],
+    packages=["nion.eels_analysis", "nion.eels_analysis.test", "nionswift_plugin.nion_eels_analysis", "nionswift_plugin.nion_eels_analysis.test"],
     package_data={"nion.eels_analysis": ["resources/*"]},
     install_requires=[],
     classifiers=[
