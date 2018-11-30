46f0a1f08f20d1f084b4d4d32b1b43d88d2a8cf7
Chris Meyer <cmeyer1969@gmail.com>
2018-11-30 09:24:39 -0800

    Update CHANGES and nionswift requirements (display item).

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,11 @@
 Changelog (nionswift-eels-analysis)
 ===================================
 
+0.2.2 (UNRELEASED)
+------------------
+
+- Use composite line plot display for display again.
+
 0.2.1 (2018-10-14)
 ------------------
 
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     url="https://github.com/nion-software/eels-analysis",
     packages=["nion.eels_analysis", "nion.eels_analysis.test", "nionswift_plugin.nion_eels_analysis", "nionswift_plugin.nion_eels_analysis.test"],
     package_data={"nion.eels_analysis": ["resources/*"]},
-    install_requires=["nionswift>=0.13.9"],
+    install_requires=["nionswift>=0.14.0"],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Programming Language :: Python :: 3.6",
