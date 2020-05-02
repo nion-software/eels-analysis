127fc8be6826895a035a9e7698044ccad3ab250a
Chris Meyer <cmeyer1969@gmail.com>
2020-05-01 17:40:40 -0700

    Switch to declarative setup.cfg + pyproject file. Require Python 3.7.

--- setup.py
+++ setup.py
@@ -1,29 +1,4 @@
-# -*- coding: utf-8 -*-
+from setuptools import setup
 
-"""
-To upload to PyPI, PyPI test, or a local server:
-python setup.py bdist_wheel upload -r <server_identifier>
-"""
-
-import setuptools
-import os
-
-setuptools.setup(
-    name="nionswift-eels-analysis",
-    version="0.4.4",
-    author="Nion Software",
-    author_email="swift@nion.com",
-    description="Library and UI for doing EELS analysis with Nion Swift.",
-    long_description=open("README.rst").read(),
-    url="https://github.com/nion-software/eels-analysis",
-    packages=["nion.eels_analysis", "nion.eels_analysis.test", "nionswift_plugin.nion_eels_analysis", "nionswift_plugin.nion_eels_analysis.test"],
-    package_data={"nion.eels_analysis": ["resources/*"]},
-    install_requires=["nionswift>=0.14.0"],
-    license='GPLv3',
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Programming Language :: Python :: 3.6",
-    ],
-    test_suite="nion.eels_analysis.test",
-    python_requires='~=3.6',
-)
+if __name__ == "__main__":
+    setup()
