9862bf44c93a6e0d20f3a77d93129d9b8a0bd2ca
MattRoyle <matthew.royle@bruker.com>
2026-07-01 15:45:29 +0100

    Refactor to set the role for measurements

--- nionswift_plugin/nion_eels_analysis/LiveZLP.py
+++ nionswift_plugin/nion_eels_analysis/LiveZLP.py
@@ -47,8 +47,7 @@ class MeasureZLP:
             zlp_interval.interval = start, end
             zlp_interval.graphic_id = "zlp_interval"
             zlp_interval._graphic.color = "black"
-            zlp_interval._graphic.text_visibility = "always"
-            zlp_interval._graphic.text_background_color = "white"
+            zlp_interval._graphic.role = "measurement"
 
 ComputationCallable = typing.Callable[[Symbolic._APIComputation], Symbolic.ComputationHandlerLike]
 
